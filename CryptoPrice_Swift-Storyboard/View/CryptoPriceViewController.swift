56c23c35ab2e05598e544a3fa5ec740c601bb639
Aleksandr <pulatov.alexander@gmail.com>
2024-01-03 16:20:21 +0300

    Added a TableView and changed the background color.

--- CryptoPrice_Swift-Storyboard/View/CryptoPriceViewController.swift
+++ CryptoPrice_Swift-Storyboard/View/CryptoPriceViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class CryptoPriceViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
